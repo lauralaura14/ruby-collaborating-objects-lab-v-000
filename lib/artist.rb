1d8ef99d36c5699916d8ee7664914ab72ca1b7f0
Laura <laurao148@gmail.com>
2018-03-21 04:16:40 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -34,7 +34,7 @@ class Artist
   def self.find_or_create_by_name(name)
     if self.find(name)
       self.find(name)
-    elsif !@@all.include?(name)
+    else
       self.create(name)
     end
   end
